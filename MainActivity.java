a947616eca1aa904e489891dab5befd4f4b3b805
ddbbsoyeon <43229447+ddbbsoyeon@users.noreply.github.com>
2018-09-25 17:02:34 +0900

    Async, HttpURLConnection Android

---
+++ MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.user.ras_app;
+
+import android.content.ContentValues;
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView tv_outPut;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // 위젯에 대한 참조
+        tv_outPut = (TextView) findViewById(R.id.tv_outPut);
+
+        //URL 설정
+        String url = "http://220.67.124.128:8080/";
+
+        //AsyncTask를 통해 HttpURLConnection 수행
+        NetworkTask networkTask = new NetworkTask(url, null);
+        networkTask.execute();
+    }
+
+    public class NetworkTask extends AsyncTask<Void, Void, String> {
+        private String url;
+        private ContentValues values;
+
+        public NetworkTask(String url, ContentValues values){
+            this.url = url;
+            this.values = values;
+        }
+
+        @Override
+        protected String doInBackground(Void... params){
+            String result; //요청 결과를 저장할 변수
+            RequestHttpURLConnection requestHttpURLConnection = new RequestHttpURLConnection();
+            result = requestHttpURLConnection.request(url, values);
+
+            return result;
+        }
+
+        @Override
+        protected void onPostExecute(String s){
+            super.onPostExecute(s);
+
+            //doInBackground()로 부터 리턴된 값이 onPostExecute의 매개변수로 넘어오므로 s를 출력한다
+            tv_outPut.setText(s);
+        }
+    }
+}
+
