9ab2d8805410476e5dfb158c3be74a460e8e5026
ddbbsoyeon <sso.yally@gmail.com>
2018-09-26 18:01:22 +0900

    android http-async

--- app/src/main/java/com/example/user/jj_ras_app/MainActivity.java
+++ app/src/main/java/com/example/user/jj_ras_app/MainActivity.java
@@ -1,64 +1,100 @@
 package com.example.user.jj_ras_app;
 
-import android.content.ContentValues;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
-import org.jsoup.nodes.Element;
-import org.jsoup.select.Elements;
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Scanner;
+
+import javax.net.ssl.HttpsURLConnection;
 
 public class MainActivity extends AppCompatActivity {
 
-    private TextView tv_outPut;
+    BackgroundTask task;
+    String address, result;
+    private TextView textView;
     private String url = "https://220.67.124.128:8080";
     private String res = "";
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // 위젯에 대한 참조
-        tv_outPut = (TextView) findViewById(R.id.tv_outPut);
+        textView = (TextView) findViewById(R.id.textView);
+        btn = (Button) findViewById(R.id.button);
 
-        //AsyncTask를 통해 HttpURLConnection 수행
-        NetworkTask networkTask = new NetworkTask();
-        networkTask.execute();
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                task = new BackgroundTask();
+                task.execute();
+            }
+        });
     }
 
-    public class NetworkTask extends AsyncTask<Void, Void, Void> {
-        @Override
-        protected void onPreExecute(){
-            super.onPreExecute();
+    class BackgroundTask extends AsyncTask<Integer, Integer, Integer> {
+        protected void onPreExecute() {
+            address = url;
         }
 
-
         @Override
-        protected Void doInBackground(Void... params){
-            try {
-                readFromStream reads = new readFromStream();
-                reads.read(reads.makeHttpRequest(url));
-                //Document doc = Jsoup.connect(url).get();
-                //Elements tables = doc.select("table.table.table-striped");
-                //System.out.println("----------------");
-                //for(Element e: tables){
-                //    res += e.text().trim();
-                //}
-            } catch (Exception e){
-                e.printStackTrace();
-            }
-            //RequestHttpURLConnection requestHttpURLConnection = new RequestHttpURLConnection();
-            //result = requestHttpURLConnection.request(url, values);
+        protected Integer doInBackground(Integer... arg0) {
+            result = request(address);
             return null;
         }
 
-        @Override
-        protected void onPostExecute(Void result){
-            tv_outPut.setText(res);
+        protected void onPostExecute(Integer a) {
+            textView.setText(result);
         }
+
     }
+
+    private String request(String urlStr) {
+        StringBuilder output = new StringBuilder();
+        try {
+            URL url = new URL(urlStr);
+            HttpURLConnection conn = (HttpURLConnection)url.openConnection();
+            if (conn != null) {
+                conn.setConnectTimeout(10000);
+                conn.setRequestMethod("GET");
+                conn.setDoInput(true);
+                conn.setDoOutput(true);
+
+                int resCode = conn.getResponseCode();
+                if (resCode == HttpURLConnection.HTTP_OK) {
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(conn.getInputStream())) ;
+                    String line = null;
+                    while(true) {
+                        line = reader.readLine();
+                        if (line == null) {
+                            break;
+                        }
+                        output.append(line + "\n");
+                    }
+
+                    reader.close();
+                    conn.disconnect();
+                }
+            }
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+
+        return output.toString();
+    }
+
 }
